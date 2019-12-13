44254d6e38f7373fa8194f6400a575453b9d9e33
474868770//git <474868770@qq.com>
2019-12-13 16:57:02 +0800

    master

--- src/components/iot.vue
+++ src/components/iot.vue
@@ -95,127 +95,142 @@
     <van-panel title="塔机升降机设备">
       <div>
         <van-row>
-          <van-col span="2">
-            <div
-              style="position:relative;margin:10px 5px 10px 5px;float: left;top:1px;left:0px;width:10px;height:10px;border-top:solid 1px #bababa;"
-            ></div>
-            <div
-              style="position:absolute;content:'';top:55px;left:10px;width:1px;height:55px;background-color:#bababa;"
-            ></div>
-            <div style="padding-top:70px;font-size:14px;color:#515151">{{this.tjHight}}m</div>
-            <div
-              style="position:relative;margin:10px 5px 10px 5px;float: left;top:53px;left:0px;width:10px;height:10px;border-top:solid 1px #bababa;"
-            ></div>
-            <div
-              style="position:absolute;content:'';top:45px;left:10px;width:1px;height:55px;background-color:#bababa;margin-top:100px"
-            ></div>
-          </van-col>
-          <van-col span="7">
-            <img src="../../static/images/taji.png" style="max-height:160px" />
-            <img src="../../static/images/shengjiangji.png" style="max-height:160px;margin-top:30px" />
-          </van-col>
-          <van-col span="2">
-            <div
-              style="position:relative;margin:10px 5px 10px 5px;float: left;top:1px;left:-10px;width:10px;height:10px;border-top:solid 1px #bababa;"
-            ></div>
-            <div
-              style="position:absolute;content:'';top:55px;width:1px;height:30px;background-color:#bababa;"
-            ></div>
-            <div style="padding-top:50px;font-size:14px;color:#515151">{{this.tdHight}}m</div>
-            <div
-              style="position:relative;margin:10px 5px 10px 5px;float: left;top:23px;left:-10px;width:10px;height:10px;border-top:solid 1px #bababa;"
-            ></div>
-            <div
-              style="position:absolute;content:'';top:20px;width:1px;height:30px;background-color:#bababa;margin-top:100px"
-            ></div>
-          </van-col>
-          <van-col span="13">
-            <div style="color:#fff;font-size:14px;font-weight:bold;background:#1ABC9C">塔机数据</div>
-            <div v-if="this.tjId != ''">
+          <van-col span="10">
+            <div>
+              <div
+                style="margin-top:80px;margin-left:10px;font-size:14px;color:#515151;position:absolute"
+              >{{this.tjHight}}m</div>
+              <div
+                style="margin:100px 0px 0px 100px;font-size:14px;color:#515151;position:absolute"
+              >{{this.tdHight}}m</div>
+              <div
+                style="margin:0px 0px 0px 60px;font-size:14px;color:#515151;position:absolute"
+              >{{this.tjJiaodu}}°</div>
+              <div
+                style="margin:30px 0px 0px 70px;font-size:14px;color:#515151;position:absolute"
+              >{{this.tjfudu}}m</div>
+              <img src="../../static/images/taji.png" style="max-height:160px" />
               <van-row>
-                <van-col span="12">
-                  <div class="kapian1">
-                    <van-row>
-                      <van-col span="12">
-                        <img
-                          src="../../static/images/jiaodu.png"
-                          style="max-width:60%;margin:5px 5px 5px 5px"
-                        />
-                      </van-col>
-                      <van-col span="12" style="text-align:left">
-                        <div style="font-size:14px;text-align:left">角度</div>
-                        <div style="font-size:14px;text-align:left">{{this.tjJiaodu}}</div>
-                      </van-col>
-                    </van-row>
-                  </div>
+                <van-col span="8">
+                  <img
+                    src="../../static/images/zhongliang.png"
+                    style="max-width:50%;margin:5px 5px 5px 5px"
+                  />
                 </van-col>
-                <van-col span="12">
-                  <div class="kapian1">
-                    <van-row>
-                      <van-col span="12">
-                        <img
-                          src="../../static/images/fengli.png"
-                          style="max-width:60%;margin:5px 5px 5px 5px"
-                        />
-                      </van-col>
-                      <van-col span="12" style="text-align:left">
-                        <div style="font-size:14px;text-align:left">风速</div>
-                        <div style="font-size:14px;text-align:left">{{this.tjfengsu}}</div>
-                      </van-col>
-                    </van-row>
-                  </div>
+                <van-col span="12" style="text-align:left">
+                  <div style="font-size:14px;text-align:left">重量:{{this.weight}}</div>
                 </van-col>
               </van-row>
-              <van-row>
-                <van-col span="12">
-                  <div class="kapian1">
-                    <van-row>
-                      <van-col span="12">
-                        <img
-                          src="../../static/images/huizhuan.png"
-                          style="max-width:60%;margin:5px 5px 5px 5px"
-                        />
-                      </van-col>
-                      <van-col span="12" style="text-align:left">
-                        <div style="font-size:14px;text-align:left">回转</div>
-                        <div style="font-size:14px;text-align:left">{{this.rotate}}</div>
-                      </van-col>
-                    </van-row>
-                  </div>
-                </van-col>
-                <van-col span="12">
-                  <div class="kapian1">
-                    <van-row>
-                      <van-col span="12">
-                        <img
-                          src="../../static/images/fudu.png"
-                          style="max-width:60%;margin:5px 5px 5px 5px"
-                        />
-                      </van-col>
-                      <van-col span="12" style="text-align:left">
-                        <div style="font-size:14px;text-align:left">幅度</div>
-                        <div style="font-size:14px;text-align:left">{{this.tjfudu}}</div>
-                      </van-col>
-                    </van-row>
-                  </div>
-                </van-col>
-              </van-row>
-              <div class="kapian1">
-                <van-row>
-                  <van-col span="8">
-                    <img
-                      src="../../static/images/zhongliang.png"
-                      style="max-width:50%;margin:5px 5px 5px 5px"
-                    />
-                  </van-col>
-                  <van-col span="12" style="text-align:left">
-                    <div style="font-size:14px;text-align:left">重量{{this.weight}}</div>
-                  </van-col>
-                </van-row>
-              </div>
-              <div style="color:#999;font-size:12px">上报时间：{{this.tjRTime}}</div>
             </div>
-            <div v-if="this.tjId ==''" style="margin:70px 30px 70px 30px">未配置塔机</div>
+            <img
+              src="../../static/images/shengjiangji.png"
+              style="max-height:160px;margin:50px 0px 0px 40px"
+            />
+          </van-col>
+          <van-col span="14">
+            <div
+              style="color:#fff;font-size:14px;font-weight:bold;background:#1ABC9C;margin-bottom:5px"
+            >塔机数据</div>
+            <van-tabs type="card" color="#1ABC9C">
+              <van-tab title="塔机 1">
+                <div v-if="this.tjId != ''">
+                  <van-row style="margin-top:5px">
+                    <van-col span="12">
+                      <div class="kapian1">
+                        <van-row>
+                          <van-col span="12">
+                            <img
+                              src="../../static/images/jiaodu.png"
+                              style="max-width:60%;margin:5px 5px 5px 5px"
+                            />
+                          </van-col>
+                          <van-col span="12" style="text-align:left">
+                            <div style="font-size:12px;text-align:left">大臂倾角</div>
+                            <div style="font-size:14px;text-align:left">{{this.tjJiaodu}}</div>
+                          </van-col>
+                        </van-row>
+                      </div>
+                    </van-col>
+                    <van-col span="12">
+                      <div class="kapian1">
+                        <van-row>
+                          <van-col span="12">
+                            <img
+                              src="../../static/images/fengli.png"
+                              style="max-width:60%;margin:5px 5px 5px 5px"
+                            />
+                          </van-col>
+                          <van-col span="12" style="text-align:left">
+                            <div style="font-size:12px;text-align:left">风速</div>
+                            <div style="font-size:14px;text-align:left">{{this.tjfengsu}}</div>
+                          </van-col>
+                        </van-row>
+                      </div>
+                    </van-col>
+                  </van-row>
+                  <van-row>
+                    <van-col span="12">
+                      <div class="kapian1">
+                        <van-row>
+                          <van-col span="12">
+                            <img
+                              src="../../static/images/huizhuan.png"
+                              style="max-width:60%;margin:5px 5px 5px 5px"
+                            />
+                          </van-col>
+                          <van-col span="12" style="text-align:left">
+                            <div style="font-size:12px;text-align:left">回转</div>
+                            <div style="font-size:14px;text-align:left">{{this.rotate}}</div>
+                          </van-col>
+                        </van-row>
+                      </div>
+                    </van-col>
+                    <van-col span="12">
+                      <div class="kapian1">
+                        <van-row>
+                          <van-col span="12">
+                            <img
+                              src="../../static/images/fudu.png"
+                              style="max-width:60%;margin:5px 5px 5px 5px"
+                            />
+                          </van-col>
+                          <van-col span="12" style="text-align:left">
+                            <div style="font-size:12px;text-align:left">幅度</div>
+                            <div style="font-size:14px;text-align:left">{{this.tjfudu}}</div>
+                          </van-col>
+                        </van-row>
+                      </div>
+                    </van-col>
+                  </van-row>
+                  <div class="kapian1">
+                    <van-row>
+                      <van-col span="8">
+                        <img
+                          src="../../static/images/zhongliang.png"
+                          style="max-width:50%;margin:5px 5px 5px 5px"
+                        />
+                      </van-col>
+                      <van-col span="12" style="text-align:left">
+                        <div style="font-size:14px;text-align:left">力矩</div>
+                        <van-progress
+                          :percentage="this.torque_percent"
+                          color="#1ABC9C"
+                          style="margin-top:10px"
+                        />
+                      </van-col>
+                    </van-row>
+                  </div>
+                  <div style="color:#999;font-size:12px">上报时间：{{this.tjRTime}}</div>
+                </div>
+                <div v-if="this.tjId ==''" style="margin:70px 30px 70px 30px">未配置塔机</div>
+              </van-tab>
+              <van-tab title="塔机 2">
+                <div style="margin:75px 50px 75px 50px">未配置塔机 2</div>
+              </van-tab>
+              <van-tab title="塔机 3">
+                <div style="margin:75px 50px 75px 50px">未配置塔机 3</div>
+              </van-tab>
+            </van-tabs>
             <div style="color:#fff;font-size:14px;font-weight:bold;background:#1ABC9C">升降机数据</div>
             <div v-if="this.shengjianId != ''">
               <div style="color:#999;font-size:14px">高度：{{this.Height}}米</div>
@@ -309,9 +324,10 @@ export default {
       tjfudu: "",
       tjJiaodu: "",
       tjfengsu: "",
-      rotate:"",
-      weight:"",
-      tdHight:"",
+      torque_percent: "",
+      rotate: "",
+      weight: "",
+      tdHight: "",
       power: "",
       powerId: "",
       toDaypower: "",
@@ -362,7 +378,7 @@ export default {
         this.show = false;
       });
 
-      //定时器更新数据
+      //定时器更新环境检测仪数据
       this.timer = setInterval(() => {
         request({
           url: util.api_url3002 + "/datum/meter",
@@ -381,6 +397,28 @@ export default {
           this.pm2_5 = res.data[0].pm2_5;
           this.huanjinglastTime = res.data[0].record_time;
           this.show = false;
+        });
+      }, 60000);
+
+      //定时器更新塔机数据
+      this.timer2 = setInterval(() => {
+        request({
+          url: util.api_url3002 + "/datum/meter",
+          method: "post",
+          data: {
+            method: "query_crane2",
+            project_id: this.projectId,
+            devid: this.tjId
+          }
+        }).then(res => {
+          this.tjRTime = res.data[0].created_time;
+          this.tjfudu = res.data[0].extent;
+          this.tjJiaodu = res.data[0].dip_angle;
+          this.tjfengsu = res.data[0].wind;
+          this.tdHight = res.data[0].height;
+          this.rotate = res.data[0].rotate;
+          this.weight = res.data[0].weight;
+          this.torque_percent = res.data[0].torque_percent;
         });
       }, 60000);
 
@@ -460,7 +498,7 @@ export default {
           } else if (res.data[i].device_type == 13) {
             this.tjId = res.data[i].device_id;
             this.tjHight = JSON.parse(res.data[i].params_json).height;
-            console.log(this.tjHight)
+            console.log(this.tjHight);
 
             //塔机数据
             request({
@@ -479,6 +517,7 @@ export default {
               this.tdHight = res.data[0].height;
               this.rotate = res.data[0].rotate;
               this.weight = res.data[0].weight;
+              this.torque_percent = res.data[0].torque_percent;
             });
           }
         }
@@ -573,6 +612,7 @@ export default {
 
   beforeDestroy() {
     clearInterval(this.timer);
+    clearInterval(this.timer2);
   }
 };
 </script>
--- src/components/person.vue
+++ src/components/person.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
     <!-- 头部人员信息 -->
+    <van-dialog
+      v-model="show"
+      :title="'请确认是否将'+userName+'更换为'+personInfo.zhuanye"
+      show-cancel-button
+      @confirm="changeB"
+      @cancel="cancelB"
+    ></van-dialog>
     <div class="title" style="background-color: #1AC9B9;">
       <van-row>
         <van-col span="8">
           <img :src="this.idcard_pic" style="margin:10px" />
         </van-col>
-        <van-col span="16">
+        <van-col span="9">
           <div style="color:#fff;font-size:18px">{{userName}}</div>
           <div class="xinxi">部门：{{banzhu}}</div>
           <div class="xinxi">专业：{{zhuanye}}</div>
@@ -15,6 +22,18 @@
           <div class="xinxi">
             电话：
             <a :href="'tel:'+mobile">{{mobile}}</a>
+          </div>
+        </van-col>
+        <van-col span="7">
+          <div class="block" style="width:100px;margin-top:30px">
+            <el-cascader
+              v-model="value"
+              :options="options"
+              @change="handleChange"
+              :show-all-levels="false"
+              placeholder="更换部门"
+              size="mini"
+            ></el-cascader>
           </div>
         </van-col>
       </van-row>
@@ -57,9 +76,15 @@
               @num-pages="pageCount=$event"
             ></pdf>
             <div>
-              <van-button @click="changePdfPage(0)" style="background-color:#1AC9B9;color:#fff;margin:10px 10px 10px 80px">上一页</van-button>
+              <van-button
+                @click="changePdfPage(0)"
+                style="background-color:#1AC9B9;color:#fff;margin:10px 10px 10px 80px"
+              >上一页</van-button>
               {{currentPage}} / {{pageCount}}
-              <van-button @click="changePdfPage(1)" style="background-color:#1AC9B9;color:#fff;margin:0 0 0 10px">下一页</van-button>
+              <van-button
+                @click="changePdfPage(1)"
+                style="background-color:#1AC9B9;color:#fff;margin:0 0 0 10px"
+              >下一页</van-button>
             </div>
           </div>
         </div>
@@ -150,11 +175,13 @@ export default {
           title: "考试试题及结果"
         }
       ],
+      value: "",
       idcard_pic: "",
       idcardPic1: "",
       idcardPic2: "",
       rzPic: "",
       zlPdf: "",
+      group: "",
       yema: 0,
       numPages: 1,
       personId: this.$route.query.id,
@@ -162,16 +189,74 @@ export default {
       loading: true,
       userName: "",
       banzhu: "",
+      group: "",
       zhuanye: "",
       age: "",
       mobile: "",
+      show: false,
       project: "",
+      showPicker: false,
       currentPage: 0, // pdf文件页码
-      pageCount: 0 // pdf文件总页数
+      pageCount: 0, // pdf文件总页数
+      options: [
+        {
+          value: "1010",
+          label: "指南",
+          children: [{ value: "1011", label: "设计原则" }]
+        },
+        {
+          value: "1010",
+          label: "指南2",
+          children: [{ value: "1011", label: "设计原则2" }]
+        }
+      ],
+      personInfo: []
     };
   },
 
   methods: {
+    handleChange(value, options) {
+      console.log(value, this.options);
+      this.show = true;
+      var temp = {};
+      for (let i in this.options) {
+        if (value[0] == this.options[i].value) {
+          for (let j in this.options[i].children)
+            if (value[1] == this.options[i].children[j].value) {
+              temp = {
+                bumen: this.options[i].label,
+                zhuanye: this.options[i].children[j].label,
+                bumenid: value[0],
+                zhuanyeid: value[1]
+              };
+            }
+        }
+      }
+      console.log(temp);
+      this.personInfo = temp;
+    },
+
+    changeB() {
+      console.log(this.personInfo);
+      request({
+        url: util.api_url3002 + "/info/project",
+        method: "post",
+        async: false,
+        data: {
+          person_id: this.personId,
+          method: "set_person_props",
+          project_id: this.projectId,
+          group_id: this.personInfo.zhuanyeid
+        }
+      }).then(res => {
+        location.reload();
+      });
+    },
+
+    cancelB() {
+      this.value = "";
+    },
+
     onClick(i) {
       console.log(i);
       this.zlPdf = "";
@@ -223,7 +308,6 @@ export default {
   mounted() {
     this.projectId = parseInt(this.$getCookie("project_id"));
     var openId = this.$getCookie("openid");
-
     request({
       url:
         util.api_url + "/api?method=xcx.project&project_id=" + this.projectId,
@@ -233,6 +317,72 @@ export default {
       data: {}
     }).then(res => {
       this.project = res.name;
+    });
+
+    request({
+      url: util.api_url3002 + "/info/project",
+      method: "post",
+      async: false,
+      data: {
+        method: "query_group",
+        project_id: this.projectId
+      }
+    }).then(res => {
+      // for (let i in res.data.group) {
+      //   if (res.data.group[i].group_name == res.data.group[i].group_name) {
+      //     for (let j in res.data.group[i].group) {
+      //       if (
+      //         res.data.group[i].group[j].groups_type ==
+      //         res.data.group[i].group[j].groups_type
+      //       ) {
+      //         test[res.data.group[i].group_name] =
+      //           res.data.group[i].group[j].group_name;
+      //       }
+      //     }
+      //   }
+      // }
+      var group = [];
+      for (let i in res.data.group) {
+        // if (res.data.group[i].group_name == res.data.group[i].group_name) {
+        var subgroup = [];
+        for (let j in res.data.group[i].group) {
+          subgroup.push({
+            label: res.data.group[i].group[j].group_name,
+            value: res.data.group[i].group[j].id
+          });
+          // }
+        }
+        group.push({
+          children: subgroup,
+          label: res.data.group[i].group_name,
+          value: res.data.group[i].id
+        });
+      }
+      console.log(group);
+      this.options = group;
+      // let _test = {};
+      // for (let i in test) {
+      //   for (let j in test[i]) {
+      //     if (test[i][j].groups_type == test[i][j].groups_type) {
+      //       _test[i] = test[i][j].group_name;
+      //     }
+      //   }
+      // }
+      // console.log(_test);
+
+      // let _temp = {};
+      // for (let i in res.data.group) {
+      //   if (res.data.group[i].group_name == res.data.group[i].group_name) {
+      //     _temp[res.data.group[i].group_name] =
+      //       res.data.group[i].group[i].group_name;
+      //   }
+      // }
+      // console.log(_temp);
+      // let group = {};
+      // for (let i in _temp) {
+      //   group = _temp[i].group_name;
+      // }
+      // console.log(group);
     });
 
     request({
@@ -255,9 +405,10 @@ export default {
         (this.zhuanye = res.data[0].group_name_level[1]),
         (this.age = res.data[0].age),
         (this.mobile = res.data[0].mobile);
-      console.log(this.user);
     });
-  }
+  },
+
+  created() {}
 };
 </script>
 
